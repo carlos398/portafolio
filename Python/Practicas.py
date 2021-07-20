b2105369af164914b87ffd9e20d9f6937c0b8b27
careyes098 <carlosarp2000@gmail.com>
2021-07-20 12:55:18 -0500

    puntuaciones de una empresa

--- Python/Practicas.py
+++ Python/Practicas.py
@@ -127,5 +127,36 @@ def renta_anual(args):
     return True
 
 
+
+"""
+    En una determinada empresa, sus empleados son evaluados al final de cada año. 
+    Los puntos que pueden obtener en la evaluación comienzan en 0.0 y pueden ir aumentando, 
+    traduciéndose en mejores beneficios. Los puntos que pueden conseguir los empleados pueden ser 0.0, 0.4, 0.6 o más, 
+    pero no valores intermedios entre las cifras mencionadas. 
+    A continuación se muestra una tabla con los niveles correspondientes a cada puntuación. 
+    La cantidad de dinero conseguida en cada nivel es de 2.400€ multiplicada por la puntuación del nivel.
+
+    Nivel	Puntuación
+    Inaceptable	0.0
+    Aceptable	0.4
+    Meritorio	0.6 o más
+    Escribir un programa que lea la puntuación del usuario e indique su nivel de rendimiento, 
+    así como la cantidad de dinero que recibirá el usuario.
+"""
+
+
+def Puntuacion():
+    puntuacion = float(input("Digite su puntuacion: "))
+    if puntuacion == 0.0:
+        print("su nivel es: Inaceptable y la cantidad de dinero que recibira es de: {0}€ ".format( 2400 * puntuacion ))
+    elif puntuacion == 0.4:
+        print("su nivel es: Aceptable y la cantidad de dinero que recibira es de: {0}€ ".format( 2400 * puntuacion ))
+    elif puntuacion == 0.6 or puntuacion > 0.6:
+        print("su nivel es: Meritorio y la cantidad de dinero que recibira es de: {0}€ ".format( 2400 * puntuacion ))
+    else:
+        print("digite una puntuacio valida")
+    return True
+
+
 if __name__ == '__main__':
-    grupos_curso()
+    Puntuacion()
