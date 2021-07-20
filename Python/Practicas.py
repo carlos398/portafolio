ea070118096cb164c58cd3c6bff29f1f5e44ea78
careyes098 <carlosarp2000@gmail.com>
2021-07-20 11:57:52 -0500

    practicando py

--- Python/Practicas.py
+++ Python/Practicas.py
@@ -62,5 +62,17 @@ def par_impar():
         print("el numero es par")
 
 
+"""
+    Para tributar un determinado impuesto se debe ser mayor de 16 años 
+    y tener unos ingresos iguales o superiores a 1000 € mensuales. 
+    Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales 
+    y muestre por pantalla si el usuario tiene que tributar o no.
+"""
+
+
+def impuestos():
+    
+
+
 if __name__ == '__main__':
-    par_impar()
+    pass
