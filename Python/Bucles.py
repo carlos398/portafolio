622b24654897c8f0ad25650e706bc23e13070565
carlos398 <carlosarp2000@gmail.com>
2021-07-28 20:32:24 -0500

    prime

--- Python/Bucles.py
+++ Python/Bucles.py
@@ -110,5 +110,21 @@ def contrasena():
     return contra
 
 
+"""
+    Escribir un programa que pida al usuario un número entero y muestre por pantalla si es un número primo o no.
+"""
+
+
+def primo():
+    n = int(input("Introduce un número entero positivo mayor que 2: "))
+    i = 2
+    while n % i != 0:
+        i += 1
+    if i == n:
+        print(str(n) + " es primo")
+    else:
+        print(str(n) + " no es primo")
+
+
 if __name__ == '__main__':
-    contrasena()
+    primo()
