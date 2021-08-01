ae7e8a18437fd6af47840c93f0d30d756da2c0bf
carlos398 <carlosarp2000@gmail.com>
2021-08-01 13:12:00 -0500

    unittest edad

--- Unite test/unittest_edad.py
+++ Unite test/unittest_edad.py
@@ -1,9 +1,9 @@
 import unittest
 import datetime
-import fecha_edad 
+from fecha_edad import edad
 
 class TestEdad(unittest.TestCase):
     def test_edad(self):
         #prueba de cuando conocemos la edad 
         print("prueba de la edad segun su año de nacimiento: ")
-        self.assertAlmostEqual()
+        self.assertAlmostEqual(edad(2000), 21)
