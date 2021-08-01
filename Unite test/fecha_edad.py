501e87e228ffc06e9444590a4726434fe43dc0e5
carlos398 <carlosarp2000@gmail.com>
2021-08-01 13:03:00 -0500

    edad y unittest pruebas

--- Unite test/fecha_edad.py
+++ Unite test/fecha_edad.py
@@ -1,15 +1,7 @@
 import datetime
 
 
-def fecha_nacimiento():
-    cadena = "Digite su fecha de nacimiento en el formato: AÑO/MES/DIA: "
-    nacimiento = input(cadena)
-
-    return nacimiento
-
-def edad():
-    nacimiento = str(fecha_nacimiento())
-    nacimiento = nacimiento[:4]
+def edad(nacimiento):
     fechaActual = datetime.datetime.now()
     fechaActual2 = datetime.datetime.strftime(fechaActual, '%Y/%m/%d')
     fechaActual2 = str(fechaActual2[:4])
@@ -18,5 +10,10 @@ def edad():
     return edad
 
 
+cadena = "Digite su fecha de nacimiento en el formato: AÑO/MES/DIA: "
+nacimiento = input(cadena)
+nacimiento = nacimiento[:4]
+
+
 if __name__ == '__main__':
-    edad()
+    print(edad(nacimiento))
---
+++ Unite test/unittest_edad.py
@@ -0,0 +1,9 @@
+import unittest
+import datetime
+import fecha_edad 
+
+class TestEdad(unittest.TestCase):
+    def test_edad(self):
+        #prueba de cuando conocemos la edad 
+        print("prueba de la edad segun su año de nacimiento: ")
+        self.assertAlmostEqual()
