7beb609a79942d3b24fab3cf4fd150568503c196
carlos398 <carlosarp2000@gmail.com>
2021-08-01 13:09:41 -0500

    correccion edad

--- Unite test/fecha_edad.py
+++ Unite test/fecha_edad.py
@@ -1,19 +1,23 @@
 import datetime
 
+def fecha_nacimiento():
+    cadena = "Digite su fecha de nacimiento en el formato: AÑO/MES/DIA: "
+    nacimiento = input(cadena)
+    nacimiento = nacimiento[:4]
+    nacimiento = int(nacimiento)
+
+    return nacimiento
+
 
 def edad(nacimiento):
     fechaActual = datetime.datetime.now()
     fechaActual2 = datetime.datetime.strftime(fechaActual, '%Y/%m/%d')
     fechaActual2 = str(fechaActual2[:4])
-    edad = int(fechaActual2) - int(nacimiento)
+    edad = int(fechaActual2) - nacimiento
 
     return edad
 
 
-cadena = "Digite su fecha de nacimiento en el formato: AÑO/MES/DIA: "
-nacimiento = input(cadena)
-nacimiento = nacimiento[:4]
-
-
 if __name__ == '__main__':
+    nacimiento = fecha_nacimiento()
     print(edad(nacimiento))
