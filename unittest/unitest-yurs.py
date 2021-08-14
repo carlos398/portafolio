32b259f864d91601fe20ada2984cb64dd512e8a8
carlos reyes <carlosarp2000@gmail.com>
2021-08-14 18:34:43 -0500

    ajustes unitest

--- unittest/unitest-yurs.py
+++ unittest/unitest-yurs.py
@@ -3,24 +3,24 @@ from ejercicio_yurs import order_by_age
 
 
 users = [
-    {'name': 'yurley', 'birthday': '1994-06-22' },
-    {'name': 'mac-gr', 'birthday': '1993-08-31' },
-    {'name': 'carlos', 'birthday': '2000-06-02' },
-    {'name': 'reyezz', 'birthday': '1994-09-21' },
-    {'name': 'dayana', 'birthday': '1992-12-27' },
-    {'name': 'maximo', 'birthday': '2011-06-02' },
-    {'name': 'manolo', 'birthday': '1997-01-26' }
+    {'name': 'pedro', 'birthday': '1990-06-22' },
+    {'name': 'ana', 'birthday': '1995-08-31' },
+    {'name': 'juancho', 'birthday': '2020-06-02' },
+    {'name': 'guacho', 'birthday': '2004-09-21' },
+    {'name': 'king', 'birthday': '2005-12-27' },
+    {'name': 'dos', 'birthday': '2001-06-02' },
+    {'name': 'rick', 'birthday': '1997-01-26' }
 ]
 
 
 users2 = [
-    {'name': 'maximo', 'birthday': '2011-06-02'},
-    {'name': 'carlos', 'birthday': '2000-06-02'},
-    {'name': 'manolo', 'birthday': '1997-01-26'},
-    {'name': 'reyezz', 'birthday': '1994-09-21'},
-    {'name': 'yurley', 'birthday': '1994-06-22'},
-    {'name': 'mac-gr', 'birthday': '1993-08-31'},
-    {'name': 'dayana', 'birthday': '1992-12-27'}
+    {'name': 'juancho', 'birthday': '2020-06-02' },
+    {'name': 'king', 'birthday': '2005-12-27' },
+    {'name': 'guacho', 'birthday': '2004-09-21' },
+    {'name': 'dos', 'birthday': '2001-06-02' },
+    {'name': 'rick', 'birthday': '1997-01-26' },
+    {'name': 'ana', 'birthday': '1995-08-31' },
+    {'name': 'pedro', 'birthday': '1990-06-22' },
 ]
 
 class TestOrderAge(unittest.TestCase):
